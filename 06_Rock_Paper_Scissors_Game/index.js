b84733abc12d8e568ab06b773287885725f510a2
Arslan Arslan <44298143+arslanaarslan@users.noreply.github.com>
2023-12-04 00:44:39 +0300

    Rock Paper Scissors Game completed.

---
+++ 06_Rock_Paper_Scissors_Game/index.js
@@ -0,0 +1,43 @@
+const buttons = document.querySelectorAll('button');
+
+const resultEl = document.getElementById("result");
+
+const playerScoreEl = document.getElementById("user-score");
+const computerScoreEl = document.getElementById("computer-score");
+
+let playerScore = 0;
+let computerScore = 0;
+
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        resultEl.textContent = playRound(button.id, computerPlay());
+    });
+});
+
+function computerPlay() {
+    const choices = ["rock", "paper", "scissors"];
+    const randomChoice = Math.floor(Math.random() * choices.length);
+    return choices[randomChoice];
+}
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        return "It's a tie!";
+    } else if (
+        (playerSelection === "rock" &&
+        computerSelection === "scissors") ||
+        (playerSelection === "paper" &&
+        computerSelection === "rock") ||
+        (playerSelection === "scissors" &&
+        computerSelection === "paper")
+    ) {
+        playerScore += 1;
+        playerScoreEl.textContent = playerScore;
+        return "You win! " + playerSelection + " beats " + computerSelection;
+    } else {
+        computerScore += 1;
+        computerScoreEl.textContent = computerScore;
+        return "You lose! " + computerSelection + " beats " + playerSelection;
+    }
+
+}
